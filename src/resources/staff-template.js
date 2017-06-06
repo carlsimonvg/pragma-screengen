6669ecd0a04764fc853e21f26de4f0ae396642f7
carlsimonvg <carlsimon.vg@gmail.com>
2017-06-06 13:26:08 +0200

    commit changes
    commit changes

--- src/resources/staff-template.js
+++ src/resources/staff-template.js
@@ -18,10 +18,6 @@ export const staffTemplate =
                 "map": "initials"
             },
             {
-                "field": "initials",
-                "map": "initials"
-            },
-            {
                 "field": "firstName",
                 "map": "firstName"
             },
