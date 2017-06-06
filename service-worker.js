4cc361095229101d4e415ebd11846de207c2a4b4
carlsimonvg <carlsimon.vg@gmail.com>
2017-06-06 13:16:38 +0200

    check in changes
    check in changes

---
+++ service-worker.js
@@ -0,0 +1,268 @@
+/**
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+// DO NOT EDIT THIS GENERATED OUTPUT DIRECTLY!
+// This file should be overwritten as part of your build process.
+// If you need to extend the behavior of the generated service worker, the best approach is to write
+// additional code and include it using the importScripts option:
+//   https://github.com/GoogleChrome/sw-precache#importscripts-arraystring
+//
+// Alternatively, it's possible to make changes to the underlying template file and then use that as the
+// new base for generating output, via the templateFilePath option:
+//   https://github.com/GoogleChrome/sw-precache#templatefilepath-string
+//
+// If you go that route, make sure that whenever you update your sw-precache dependency, you reconcile any
+// changes made to this original template file with your modified copy.
+
+// This generated service worker JavaScript will precache your site's resources.
+// The code needs to be saved in a .js file at the top-level of your site, and registered
+// from your pages in order to be used. See
+// https://github.com/googlechrome/sw-precache/blob/master/demo/app/js/service-worker-registration.js
+// for an example of how you can register this script and handle various service worker events.
+
+/* eslint-env worker, serviceworker */
+/* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
+'use strict';
+
+var precacheConfig = [["app/src/app.html","d0ff1d1ebdc40bf89caa3273294fa967"],["app/src/app.js","12936e93e6957aee17f961f34ab0f3d5"],["app/src/app.js.map","6bee3800b38da2e03d8e07f7900e0651"],["app/src/components/icons/icons.html","ad48b58c8556e095d9737fe61fe55a71"],["app/src/main.js","471a2492f4364ab67ead0fdbc0cf5dde"],["app/src/main.js.map","0e8af5e0430da53545b5b67c922c0c17"],["app/src/resources/staff-data.js","e4241efcbeb8dac8398290fbe9385309"],["app/src/resources/staff-data.js.map","e8e43106f5c253c8297f9fff1873142e"],["app/src/resources/staff-grouping.js","a0e61280a85184587c3f42bd2ac4d9ed"],["app/src/resources/staff-grouping.js.map","37813598ac68bbc22aaffd6d5d66afaf"],["app/src/resources/staff-template.js","fe3346dd7872461f4b44fd2eaef42617"],["app/src/resources/staff-template.js.map","1381ec9131f81b6264c815f6f8d07b60"],["app/src/vendor-build.js","aa97d6d91118bb47062f9fd2900896c9"],["app/src/views/welcome/welcome.html","81c8212b6a2c8baad83d0c36799bdaef"],["app/src/views/welcome/welcome.js","24dbd4d927760f7fa0835ffb233689bd"],["app/src/views/welcome/welcome.js.map","b3c784869abe1f32ea8eb1f8c02e0d6a"],["config.js","b65bfd1e47e78c02417b62d07dd3c2a3"],["images/app.svg","c91b40bf4094f2a92c683600b49d58fc"],["index.html","10068e2794397279aab2164dad1d181e"],["jspm_packages/system.js","eccc019329febb5a1b06bde008ca5614"],["styles/desktop.css","e132bd577d14bdffd7bf0c99d940a10f"],["styles/mobile.css","4eb7bdcbc0662a3df928af1c261b537b"],["styles/style.css","c47c4a99c3dd1064fd734ee3e3d4eac5"]];
+var cacheName = 'sw-precache-v3-untitled-' + (self.registration ? self.registration.scope : '');
+
+
+var ignoreUrlParametersMatching = [/^utm_/];
+
+
+
+var addDirectoryIndex = function (originalUrl, index) {
+    var url = new URL(originalUrl);
+    if (url.pathname.slice(-1) === '/') {
+      url.pathname += index;
+    }
+    return url.toString();
+  };
+
+var cleanResponse = function (originalResponse) {
+    // If this is not a redirected response, then we don't have to do anything.
+    if (!originalResponse.redirected) {
+      return Promise.resolve(originalResponse);
+    }
+
+    // Firefox 50 and below doesn't support the Response.body stream, so we may
+    // need to read the entire body to memory as a Blob.
+    var bodyPromise = 'body' in originalResponse ?
+      Promise.resolve(originalResponse.body) :
+      originalResponse.blob();
+
+    return bodyPromise.then(function(body) {
+      // new Response() is happy when passed either a stream or a Blob.
+      return new Response(body, {
+        headers: originalResponse.headers,
+        status: originalResponse.status,
+        statusText: originalResponse.statusText
+      });
+    });
+  };
+
+var createCacheKey = function (originalUrl, paramName, paramValue,
+                           dontCacheBustUrlsMatching) {
+    // Create a new URL object to avoid modifying originalUrl.
+    var url = new URL(originalUrl);
+
+    // If dontCacheBustUrlsMatching is not set, or if we don't have a match,
+    // then add in the extra cache-busting URL parameter.
+    if (!dontCacheBustUrlsMatching ||
+        !(url.pathname.match(dontCacheBustUrlsMatching))) {
+      url.search += (url.search ? '&' : '') +
+        encodeURIComponent(paramName) + '=' + encodeURIComponent(paramValue);
+    }
+
+    return url.toString();
+  };
+
+var isPathWhitelisted = function (whitelist, absoluteUrlString) {
+    // If the whitelist is empty, then consider all URLs to be whitelisted.
+    if (whitelist.length === 0) {
+      return true;
+    }
+
+    // Otherwise compare each path regex to the path of the URL passed in.
+    var path = (new URL(absoluteUrlString)).pathname;
+    return whitelist.some(function(whitelistedPathRegex) {
+      return path.match(whitelistedPathRegex);
+    });
+  };
+
+var stripIgnoredUrlParameters = function (originalUrl,
+    ignoreUrlParametersMatching) {
+    var url = new URL(originalUrl);
+    // Remove the hash; see https://github.com/GoogleChrome/sw-precache/issues/290
+    url.hash = '';
+
+    url.search = url.search.slice(1) // Exclude initial '?'
+      .split('&') // Split into an array of 'key=value' strings
+      .map(function(kv) {
+        return kv.split('='); // Split each 'key=value' string into a [key, value] array
+      })
+      .filter(function(kv) {
+        return ignoreUrlParametersMatching.every(function(ignoredRegex) {
+          return !ignoredRegex.test(kv[0]); // Return true iff the key doesn't match any of the regexes.
+        });
+      })
+      .map(function(kv) {
+        return kv.join('='); // Join each [key, value] array into a 'key=value' string
+      })
+      .join('&'); // Join the array of 'key=value' strings into a string with '&' in between each
+
+    return url.toString();
+  };
+
+
+var hashParamName = '_sw-precache';
+var urlsToCacheKeys = new Map(
+  precacheConfig.map(function(item) {
+    var relativeUrl = item[0];
+    var hash = item[1];
+    var absoluteUrl = new URL(relativeUrl, self.location);
+    var cacheKey = createCacheKey(absoluteUrl, hashParamName, hash, false);
+    return [absoluteUrl.toString(), cacheKey];
+  })
+);
+
+function setOfCachedUrls(cache) {
+  return cache.keys().then(function(requests) {
+    return requests.map(function(request) {
+      return request.url;
+    });
+  }).then(function(urls) {
+    return new Set(urls);
+  });
+}
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return setOfCachedUrls(cache).then(function(cachedUrls) {
+        return Promise.all(
+          Array.from(urlsToCacheKeys.values()).map(function(cacheKey) {
+            // If we don't have a key matching url in the cache already, add it.
+            if (!cachedUrls.has(cacheKey)) {
+              var request = new Request(cacheKey, {credentials: 'same-origin'});
+              return fetch(request).then(function(response) {
+                // Bail out of installation unless we get back a 200 OK for
+                // every request.
+                if (!response.ok) {
+                  throw new Error('Request for ' + cacheKey + ' returned a ' +
+                    'response with status ' + response.status);
+                }
+
+                return cleanResponse(response).then(function(responseToCache) {
+                  return cache.put(cacheKey, responseToCache);
+                });
+              });
+            }
+          })
+        );
+      });
+    }).then(function() {
+      
+      // Force the SW to transition from installing -> active state
+      return self.skipWaiting();
+      
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var setOfExpectedUrls = new Set(urlsToCacheKeys.values());
+
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.keys().then(function(existingRequests) {
+        return Promise.all(
+          existingRequests.map(function(existingRequest) {
+            if (!setOfExpectedUrls.has(existingRequest.url)) {
+              return cache.delete(existingRequest);
+            }
+          })
+        );
+      });
+    }).then(function() {
+      
+      return self.clients.claim();
+      
+    })
+  );
+});
+
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.method === 'GET') {
+    // Should we call event.respondWith() inside this fetch event handler?
+    // This needs to be determined synchronously, which will give other fetch
+    // handlers a chance to handle the request if need be.
+    var shouldRespond;
+
+    // First, remove all the ignored parameters and hash fragment, and see if we
+    // have that URL in our cache. If so, great! shouldRespond will be true.
+    var url = stripIgnoredUrlParameters(event.request.url, ignoreUrlParametersMatching);
+    shouldRespond = urlsToCacheKeys.has(url);
+
+    // If shouldRespond is false, check again, this time with 'index.html'
+    // (or whatever the directoryIndex option is set to) at the end.
+    var directoryIndex = 'index.html';
+    if (!shouldRespond && directoryIndex) {
+      url = addDirectoryIndex(url, directoryIndex);
+      shouldRespond = urlsToCacheKeys.has(url);
+    }
+
+    // If shouldRespond is still false, check to see if this is a navigation
+    // request, and if so, whether the URL matches navigateFallbackWhitelist.
+    var navigateFallback = '';
+    if (!shouldRespond &&
+        navigateFallback &&
+        (event.request.mode === 'navigate') &&
+        isPathWhitelisted([], event.request.url)) {
+      url = new URL(navigateFallback, self.location).toString();
+      shouldRespond = urlsToCacheKeys.has(url);
+    }
+
+    // If shouldRespond was set to true at any point, then call
+    // event.respondWith(), using the appropriate cache key.
+    if (shouldRespond) {
+      event.respondWith(
+        caches.open(cacheName).then(function(cache) {
+          return cache.match(urlsToCacheKeys.get(url)).then(function(response) {
+            if (response) {
+              return response;
+            }
+            throw Error('The cached response that was expected is missing.');
+          });
+        }).catch(function(e) {
+          // Fall back to just fetch()ing the request if some unexpected error
+          // prevented the cached response from being valid.
+          console.warn('Couldn\'t serve response for "%s" from cache: %O', event.request.url, e);
+          return fetch(event.request);
+        })
+      );
+    }
+  }
+});
+
+
+
+
+
+
+
--- src/resources/staff-template.js
+++ src/resources/staff-template.js
@@ -1,15 +1,359 @@
 export const staffTemplate =
-{
-    "type": "view",
-    "fields": [
-    ],
-    "body": {
-        "tabsheet": [
+    {
+        "type": "view",
+        "fields": [{
+                "field": "staffCode",
+                "map": "code"
+            },
             {
-                "id": "tabHeader",
-                "title": "Header",
-                "groups": []
+                "field": "staffDescription",
+                "map": "description"
+            },
+            {
+                "field": "siteCode",
+                "map": "siteCode"
+            },
+            {
+                "field": "initials",
+                "map": "initials"
+            },
+            {
+                "field": "initials",
+                "map": "initials"
+            },
+            {
+                "field": "firstName",
+                "map": "firstName"
+            },
+            {
+                "field": "lastName",
+                "map": "lastName"
+            },
+            {
+                "field": "jobTitle",
+                "map": "jobTitle"
+            },
+            {
+                "field": "postNumber",
+                "map": "postNumber"
+            },
+            {
+                "field": "id",
+                "map": "id"
+            },
+            {
+                "field": "phoneNumber",
+                "map": "phoneNumber"
+            },
+            {
+                "field": "mobile",
+                "map": "mobile"
+            },
+            {
+                "field": "email",
+                "map": "email"
+            },
+            {
+                "field": "address1",
+                "map": "address1"
+            },
+            {
+                "field": "address2",
+                "map": "address2"
+            },
+            {
+                "field": "address3",
+                "map": "address3"
+            },
+            {
+                "field": "tradeCode",
+                "map": "tradeCode"
+            },
+            {
+                "field": "costElementCode",
+                "map": "costElementCode"
+            },
+            {
+                "field": "calendarCode",
+                "map": "calendarCode"
+            },
+            {
+                "field": "currencyCode",
+                "map": "currencyCode"
+            },
+            {
+                "field": "sectionCode",
+                "map": "sectionCode"
+            },
+            {
+                "field": "normalTime",
+                "map": "normalTime"
+            },
+            {
+                "field": "overtime1",
+                "map": "overtime1"
+            },
+            {
+                "field": "overtime2",
+                "map": "overtime2"
+            },
+            {
+                "field": "overtime3",
+                "map": "overtime3"
+            },
+            {
+                "field": "isEmailWorkOrderApproval",
+                "map": "isEmailWorkOrderApproval"
+            },
+            {
+                "field": "isSmsWorkOrderApproval",
+                "map": "isSmsWorkOrderApproval"
+            },
+            {
+                "field": "isSmsForCriticalAssetsOnly",
+                "map": "isSmsForCriticalAssetsOnly"
             }
-        ]
-    }
-};
+        ],
+        "body": {
+            "elements": [
+                {
+                    "element": "h1",
+                    "content": "Header",
+                    "styles": ["group-header"]
+                },
+                {
+                    "element": "group",
+                    "id": "headerGroup",
+                    "title": "Header Information",
+                    "elements": [{
+                        element: "input",
+                        "title": "Code",
+                        "field": "staffCode",
+                        "attributes": {
+                            "required": "true"
+                        }
+                    },
+                        {
+                            element: "input",
+                            "title": "Description",
+                            "field": "staffDescription",
+                            "attributes": {
+                                "readonly": "true"
+                            }
+                        },
+                        {
+                            element: "input",
+                            "title": "Site",
+                            "field": "siteCode",
+                            "description": "siteDescription"
+                        }]
+                },
+                {
+                    "element": "h1",
+                    "content": "Personal Info",
+                    "styles": ["group-header"]
+                },
+                {
+                    "element": "group",
+                    "id": "personalDetailsGroup",
+                    "title": "Personal Details",
+                    "elements": [
+                        {
+                            element: "input",
+                            "title": "Initials",
+                            "field": "initials"
+                        },
+                        {
+                            element: "input",
+                            "title": "First name",
+                            "field": "firstName",
+                            "attributes": {
+                                "required": "true"
+                            }
+                        },
+                        {
+                            element: "input",
+                            "title": "Last name",
+                            "field": "lastName",
+                            "attributes": {
+                                "required": "true"
+                            }
+                        },
+                        {
+                            element: "input",
+                            "title": "Job title",
+                            "field": "jobTitle"
+                        },
+                        {
+                            element: "input",
+                            "title": "Post number",
+                            "field": "postNumber"
+                        },
+                        {
+                            element: "input",
+                            "title": "Personal number",
+                            "field": "id"
+                        }]
+                },
+                {
+                    "element": "group",
+                    id: "contactInfoGroup",
+                    title: "Contact Info",
+                    "elements": [{
+                        element: "input",
+                        "title": "Phone",
+                        "field": "phoneNumber"
+                    },
+                        {
+                            element: "input",
+                            "title": "Mobile",
+                            "field": "mobile"
+                        },
+                        {
+                            element: "input",
+                            "title": "Email",
+                            "field": "email"
+                        },
+                        {
+                            element: "input",
+                            "title": "Address 1",
+                            "field": "address1"
+                        },
+                        {
+                            element: "input",
+                            "title": "Address 2",
+                            "field": "address2"
+                        },
+                        {
+                            element: "input",
+                            "title": "Address 3",
+                            "field": "address3"
+                        }]
+                },
+                {
+                    "element": "h1",
+                    "content": "Maintenance Manager",
+                    "styles": ["group-header"]
+                },
+                {
+                    "element": "group",
+                    "id": "maintenanceManagerGroup",
+                    "title": "Maintenance Manager",
+                    "elements": [{
+                        element: "input",
+                        "title": "Section",
+                        "field": "sectionCode",
+                        "attributes": {
+                            "required": "true"
+                        },
+                        "description": "sectionDescription"
+                    },
+                        {
+                            element: "input",
+                            "title": "Trade",
+                            "field": "tradeCode",
+                            "attributes": {
+                                "required": "true"
+                            },
+                            "description": "tradeDescription"
+                        },
+                        {
+                            element: "input",
+                            "title": "Cost Element",
+                            "field": "costElementCode",
+                            "attributes": {
+                                "required": "true"
+                            },
+                            "description": "costElementDescription"
+                        },
+                        {
+                            element: "input",
+                            "title": "Calendar",
+                            "field": "calendarCode",
+                            "attributes": {
+                                "required": "true"
+                            },
+                            "description": "calendarDescription"
+                        }]
+                },
+                {
+                    "element": "group",
+                    id: "RatesPerHour",
+                    title: "Rates per hour",
+                    "elements": [{
+                        element: "input",
+                        "title": "Site currency",
+                        "field": "currencyCode",
+                        "attributes": {
+                            "readonly": "true"
+                        },
+                        "description": "currencyDescription"
+                    },
+                        {
+                            element: "input",
+                            "title": "Normal time",
+                            "field": "normalTime",
+                            "attributes": {
+                                "required": "true"
+                            }
+                        },
+                        {
+                            element: "input",
+                            "title": "Overtime 1",
+                            "field": "overtime1",
+                            "attributes": {
+                                "required": "true"
+                            }
+                        },
+                        {
+                            element: "input",
+                            "title": "Overtime 2",
+                            "field": "overtime2",
+                            "attributes": {
+                                "required": "true"
+                            }
+                        },
+                        {
+                            element: "input",
+                            "title": "Overtime 3",
+                            "field": "overtime3",
+                            "attributes": {
+                                "required": "true"
+                            }
+                        }]
+                },
+                {
+                    "element": "group",
+                    id: "Notifications",
+                    title: "Notifications",
+                    "elements": [{
+                        element: "checkbox",
+                        "title": "Email on Work Order Approval",
+                        "field": "isEmailWorkOrderApproval"
+                    }, {
+                        element: "checkbox",
+                        "title": "SMS on Work Order Approval",
+                        "field": "isSmsWorkOrderApproval"
+                    }, {
+                        element: "checkbox",
+                        "title": "SMS for Critical Assets only",
+                        "field": "isSmsForCriticalAssetsOnly"
+                    }]
+                },
+                {
+                    "element": "h1",
+                    "content": "Geographic Data",
+                    "styles": ["group-header"]
+                },
+                {
+                    "element": "group",
+                    id: "GeographicLocation",
+                    title: "Geographic location"
+                },
+                {
+                    "element": "group",
+                    id: "GeographicNotes",
+                    title: "Geographic notes"
+                }]
+        }
+    };
